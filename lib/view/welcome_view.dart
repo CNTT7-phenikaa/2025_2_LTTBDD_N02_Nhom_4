3ec9073717b9aabdec2ba2e580deaea6aa194afb
Phuonganh005 <phamthiphuonganh2315@gmail.com>
2026-03-03 09:57:18 +0000

    update welcome

---
+++ lib/view/welcome_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class WelcomeView extends StatelessWidget {
+  const WelcomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            
+            Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //ảnh
+            Image.asset('imgs/vietnam.jpg',
+            width: 200,
+            height: 200,
+            ),
+            const SizedBox(height: 10,),
+            //chữ
+            const Text(
+              'Welcome to Viet Nam',
+              style: TextStyle(fontSize: 19),
+            ),
+              ],
+            )
+            ),
+          
+            //nút
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: (){},
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                  )
+              ),
+            ),
+            const SizedBox(height: 30),
+            
+          ],
+        ),
+      )
+    );
+  }
+}
